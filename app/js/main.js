6b8698f41fae87e9aa26175c66edb610d4703b8a
ThunderInTheGrass <propeller8924@gmail.com>
2023-02-19 21:35:41 +0800

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+//bubble sort
+//3917
+function bubbleSort(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let k = 0; k < arr.length - i - 1; k++) {
+      // count++;
+      // console.log(`count ${count}`);
+      console.log(i);
+      if (arr[k] > arr[k + 1]) {
+        [arr[k], arr[k + 1]] = [arr[k + 1], arr[k]];
+      }
+    }
+  }
+  // console.log(count);
+  return arr;
+};
+console.log(bubbleSort([10,9,8,7,6,5,4,3,2,1]));
