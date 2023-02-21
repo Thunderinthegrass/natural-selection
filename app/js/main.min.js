e54674266f25d9d890f491ab032f98f01be0d17c
ThunderInTheGrass <propeller8924@gmail.com>
2023-02-21 13:33:41 +0800

    ---

--- app/js/main.min.js
+++ app/js/main.min.js
@@ -1,10 +1,12 @@
+//нахождение нечетных элементов рекурсивным методом
 function arrSearch(arr) {
   let result = [];
 
   function innerFunc(arr) {
     if (arr.length === 0) {
       return;
-    } else if (arr[0] % 2 != 0) {
+    } 
+    else if (arr[0] % 2 != 0) {
       result.push(arr[0]);
     }
     innerFunc(arr.slice(1));
@@ -15,3 +17,56 @@ function arrSearch(arr) {
 }
 
 console.log(arrSearch([1, 2, 3, 4, 5, 6, 7, 8]));
+
+function f1(arr) {
+  let result = [];
+
+  function f2(arr) {
+    if (arr.length === 0) {
+      return;
+    }
+    else if (arr[0] % 2 != 0) {
+      result.push(arr[0]);
+    }
+    f2(arr.slice(1));
+  }
+  f2(arr);
+  return result;
+}
+console.log(f1([1, 2, 3, 4, 5, 6, 7, 8]));
+
+function ff2(arr) {
+  let result = [];
+
+  function ff3(arr) {
+    if (arr.length === 0) {
+      return;
+    }
+    else if (arr[0] % 2 != 0) {
+      result.push(arr[0]);
+    }
+    ff3(arr.slice(1));
+  }
+  ff3(arr);
+  return result;
+};
+
+console.log(ff2([1,2,3,4,5,6,7,8,9,10]));
+
+function rec(arr) {
+  let result = [];
+
+  function rec1(arr) {
+    if (arr.length === 0) {
+      return;
+    }
+    else if (arr[1] % 2 != 0) {
+      result.push(arr[0]);
+    }
+    rec1(arr.slice(1));
+  }
+  rec1(arr);
+
+  return result;
+}
+console.log(rec([1,2,3,4,5,6,7,8]));
