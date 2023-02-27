3a0bbf0fbc1acf010e6e62db403b1d2721c923ac
ThunderInTheGrass <propeller8924@gmail.com>
2023-02-27 19:51:35 +0800

    gotovo

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,29 @@
-let btns = document.querySelectorAll('.modal-btn');
-let modalsOverlay = document.querySelectorAll('.modal-overlay');
+let btns = document.querySelectorAll(".modal-btn");
+let modalOverlay = document.querySelector(".modal-overlay");
+let modals = document.querySelectorAll(".modal");
+let closeBtn = document.querySelector(".close-modal-btn");
 
-btns.forEach(elem => {
-  elem.addEventListener
+btns.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    let path = e.currentTarget.getAttribute("data-path");
+
+    modals.forEach((elem) => {
+      elem.classList.remove("modal--visible");
+    });
+
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add("modal--visible");
+    modalOverlay.classList.add("modal-overlay--visible");
+  });
+});
+
+modalOverlay.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target == modalOverlay || e.target == closeBtn) {
+    modalOverlay.classList.remove("modal-overlay--visible");
+    modals.forEach((elem) => {
+      elem.classList.remove("modal--visible");
+    });
+  }
 });
